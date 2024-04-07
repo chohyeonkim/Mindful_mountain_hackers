c7340406f5c643cac90172f20b139f3877ba947e
dtk72 <dtk7251@gmail.com>
2024-04-06 19:39:35 -0700

    Add exercise api call

--- server/project/settings.py
+++ server/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Loading environmnent variables
+dotenv.load_dotenv()
--- server/project/urls.py
+++ server/project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from apps.views import ActivityDetailView
 from apps.views import ArcteryxProductsListView
-from . import views
+from apps import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
